c132219386c4c2b02c62889daa29ff0f1c959c85
k2albert123 <k2albert123@gmail.com>
2025-03-14 09:58:41 +0200

    Initial commit - MQTT light control

---
+++ light_simulation.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+
+BROKER = "test.mosquitto.org"  # Public MQTT broker
+TOPIC = "/student_group/light_control"
+
+def on_message(client, userdata, msg):
+    payload = msg.payload.decode()
+    if payload == "ON":
+        print("💡 Light is TURNED ON")
+    elif payload == "OFF":
+        print("💡 Light is TURNED OFF")
+
+client = mqtt.Client()
+client.on_message = on_message
+
+client.connect(BROKER, 1883, 60)
+client.subscribe(TOPIC)
+
+print("Listening for MQTT messages...")
+client.loop_forever()
